c3635c656b8427063ee206078ff43b16a6859bd9
Lachlan Tweedie <lachlan@spaceship.com.au>
2020-01-28 09:02:53 +1100

    Sorting - bubble, selection, merge

---
+++ sorting/index.js
@@ -0,0 +1,65 @@
+/**
+ * Bubble sort: T = O(n^2), S = O(1)
+ */
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr;
+}
+
+/**
+ * Selection sort: T = O(n^2), S = O(1)
+ */
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[indexOfMin] > arr[j]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let t = arr[i];
+      arr[i] = arr[indexOfMin];
+      arr[indexOfMin] = t;
+    }
+  }
+  return arr;
+}
+
+/**
+ * Merge sort: T = O(n log n), S = O(n)
+ */
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
